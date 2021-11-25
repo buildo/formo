485e49bc1eaef573fbaffa6c83753e6533892930
Giovanni Gonzaga <giovanni@buildo.io>
2021-11-25 17:04:32 +0100

    add utility types and rewrite some types

--- src/FieldProps.ts
+++ src/FieldProps.ts
@@ -1,11 +1,9 @@
-import { Option } from "fp-ts/Option";
-
 export type FieldProps<V, Label, FieldError> = {
   name: string;
   value: V;
   onChange: (value: V) => unknown;
   onBlur: () => unknown;
   label: Label;
-  issues: Option<FieldError>;
+  issues?: FieldError;
   disabled?: boolean;
 };
