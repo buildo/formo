9ab418d4fb6b75e37d3558440242b0c59c2b3389
matteo-pellegrino <matteo.pellegrino@buildo.io>
2021-12-03 16:12:05 +0100

    Add hook refreshing when state changes

--- src/useRefReducer.ts
+++ src/useRefReducer.ts
@@ -1,4 +1,10 @@
-import { MutableRefObject, useCallback, useEffect, useRef } from "react";
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 type Dispatch<A> = (value: A) => void;
 
@@ -18,6 +24,12 @@ type RefReducerAction<R extends RefReducer<any, any>> = R extends RefReducer<
   ? A
   : never;
 
+/*
+ * This reducer is a combination of standard useReducer and useRef.
+ * It allows you to use a reducer with a mutable ref, avoiding staleness issues on function closures.
+ * The returned state does not change on each render, but its .current property does.
+ * Differently from useRef, this hook re-renders when the state changes.
+ */
 export const useRefReducer: <R extends RefReducer<any, any>>(
   refReducer: R,
   initialState: RefReducerState<R>
@@ -26,11 +38,13 @@ export const useRefReducer: <R extends RefReducer<any, any>>(
   initialState
 ) => {
   const state = useRef(initialState);
+  const [refresh, refreshHook] = useState(false);
 
   const setState: <R extends RefReducer<any, any>>(
     action: RefReducerAction<R>
   ) => void = (action) => {
     state.current = refReducer(state.current, action);
+    refreshHook(!refresh);
   };
 
   const dispatch = useCallback(setState, [refReducer]);
